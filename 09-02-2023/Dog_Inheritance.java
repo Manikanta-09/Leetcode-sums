cc0269bcb1bd867801a0d3b528e87b848f24b19c
Manikanta-09 <85672054+Manikanta-09@users.noreply.github.com>
2023-02-10 22:14:07 +0530

    Create Dog_Inheritance.java

---
+++ 09-02-2023/Dog_Inheritance.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+class Dog{
+    private String name;
+    Dog(String name){
+        this.name =  name;
+    }
+    public String getName(){
+        return name;
+    }
+}
+class Greyhound extends Dog{
+    private String color;
+    private int weight;
+    Greyhound(String name ,String color,int weight){
+        super(name);
+        this.color = color;
+        this.weight = weight;
+    }
+    public String getColor(){
+        return color;
+    }
+    public int getWeight(){
+        return weight;
+    }
+    public String toString(){
+        return getName()+" is "+color+" in color and his weight is "+weight+"Kg";
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String name = in.nextLine();
+        String color = in.nextLine();
+        int weight = in.nextInt();
+        Greyhound pup = new Greyhound(name, color, weight);
+        System.out.println(pup.getName());
+        System.out.println(pup.getColor());
+        System.out.println(pup.getWeight());
+        System.out.println(pup);
+        System.out.println("Superclass:"+pup.getClass().getSuperclass().getName());
+        
+    }
+}
